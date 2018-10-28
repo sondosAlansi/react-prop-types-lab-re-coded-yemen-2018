b7afdb1788f62a632d80186de2ac0917f2512990
sondos alansi <sondos.alansi@yahoo.com>
2018-10-28 21:00:50 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -34,7 +34,7 @@ function range(props, propName) {
 
 
   let weight = props[propName];
-
+  
  if (!(weight >=80 && weight <= 300)) {
       return new Error( `The 'weight'prop should range between 80 and 300`);
   }
@@ -42,9 +42,7 @@ function range(props, propName) {
   if(weight===""){
     return new Error(`The 'weight'prop is required`);
   }
-  if(isNaN(weight)){
-     return new Error(`The 'weight' prop should be a number`);
-   }
+
 
 
 
