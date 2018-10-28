2a7cd3162fed0613f447fc17b3be1ed7f8dd238c
sondos alansi <sondos.alansi@yahoo.com>
2018-10-28 21:06:18 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -31,9 +31,7 @@ Product.propTypes = {
 };
 function range(props, propName) {
   let weight = props[propName];
-  if(isNaN(weight)){
-     return new Error(`The 'weight' prop should be a number`);
-   }
+
  if (!(weight >=80 && weight <= 300)) {
       return new Error( `The 'weight'prop should range between 80 and 300`);
   }
@@ -41,6 +39,9 @@ function range(props, propName) {
   if(weight===""){
     return new Error(`The 'weight'prop is required`);
   }
+  if(isNaN(weight)){
+     return new Error(`The 'weight' prop should be a number`);
+   }
 
 
 
