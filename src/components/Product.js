10b23a381e6b14c45aa048dfb548fadfcad3ac72
sondos alansi <sondos.alansi@yahoo.com>
2018-10-28 21:18:23 +0000

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,52 @@
 // Code Product Component Here
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+export default class Product  extends Component {
+
+  render(){
+
+  return(
+        <ul>
+          <li>{this.props.name }</li>
+          <li>{this.props.producer}</li>
+          <li>{this.props.hasWatermark}</li>
+          <li>{this.props.color}.</li>
+          <li>{this.props.weight}.</li>
+        </ul>
+     );
+  }
+}
+Product.defaultProps = {
+  hasWatermark: false,
+};
+Product.propTypes = {
+  name:PropTypes.string.isRequired,
+  producer:PropTypes.string,
+   hasWatermark:PropTypes.bool,
+  color:PropTypes.oneOf(['white', 'eggshell-white','salmon']).isRequired,
+
+  weight:range
+};
+function range(props, propName) {
+  let weight = props[propName];
+  if(weight===undefined){
+     return new Error(`The 'weight'prop is required`);
+   }
+  if(isNaN(weight)){
+     return new Error(`The 'weight' prop is not a number.`);
+   }
+ if (!(weight >=80 && weight <= 300)) {
+      return new Error( `The 'weight'prop should range between 80 and 300`);
+  }
+
+
+
+
+
+
+
+
+
+}
